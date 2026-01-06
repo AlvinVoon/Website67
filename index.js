8899d0cef3afc4d0490725d923ab85f35295e705
AlvinVoon <62414477+AlvinVoon@users.noreply.github.com>
2026-01-06 12:26:01 +0800

    workingv3

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
 import * as Combinatorics from 'https://cdn.jsdelivr.net/npm/js-combinatorics@2.1.2/combinatorics.min.js';
-import { animate, stagger, splitText } from './animejs/dist/bundles/anime.esm.min.js';
-
-
-const { chars } = splitText('h2', { words: false, chars: true });
-
-animate(chars, {
-  // Property keyframes
-  y: [
-    { to: '-2.75rem', ease: 'outExpo', duration: 600 },
-    { to: 0, ease: 'outBounce', duration: 800, delay: 100 }
-  ],
-  // Property specific parameters
-  rotate: {
-    from: '-1turn',
-    delay: 0
-  },
-  delay: stagger(50),
-  ease: 'inOutCirc',
-  loopDelay: 1000,
-  loop: true
-});
 
 document.addEventListener('DOMContentLoaded', () => {
     const generateBtn = document.querySelector('.generate-btn');
