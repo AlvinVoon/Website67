b2e6090c58c3efc1b3b6eb88e9a8c798fbbe7ddb
AlvinVoon <62414477+AlvinVoon@users.noreply.github.com>
2026-05-25 14:48:07 +0800

    fix_security

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ import generateMotionMachine from './motionMachine.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
 import { getFirestore, addDoc, collection, updateDoc, doc } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
-import { firebaseConfig } from './env.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBlqiRasyTXBWzivMtLZkeS1p2SPgoymKY",
+    authDomain: "website67-eabea.firebaseapp.com",
+    projectId: "website67-eabea",
+    storageBucket: "website67-eabea.firebasestorage.app",
+    messagingSenderId: "108324128833",
+    appId: "1:108324128833:web:4ce0d93007037c51a79ec0",
+    measurementId: "G-60J32Z9YP4"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
