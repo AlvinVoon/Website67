966d73ac001b4145cbdaf44b071e034353745165
AlvinVoon <62414477+AlvinVoon@users.noreply.github.com>
2026-08-17 09:44:56 +0800

    v1.1

--- sketch.js
+++ sketch.js
@@ -8,12 +8,20 @@ let lastYaw = null;
 let lastPitch = null;
 let lastRoll = null;
 
+let ABase = [];
+let BBase = [];
+
+let saved;
+
+let index = 0;
+
 let animatedList = [];
 const cameraX = document.querySelector('#cameraX');
 const cameraY = document.querySelector('#cameraY');
 const cameraZ = document.querySelector('#cameraZ');
 const keyFrameBtn = document.querySelector('#keyframe');
 const playBtn = document.querySelector('#play');
+const saveBtn = document.querySelector('#save');
 
 const addBtn = document.querySelector('#addOperater');
 const dotproductBtn = document.querySelector('#dotOperater');
@@ -26,7 +34,102 @@ const vector2Length = document.querySelector('#vector2length');
 
 const DisplayList = document.querySelector('.list');
 
-let questionIndex;
+const previousBtn = document.querySelector('.previousBtn');
+const forwardBtn = document.querySelector('.forwardBtn');
+
+const observerA = document.querySelector('#observerA');
+const observerB = document.querySelector('#observerB');
+
+const questionTextbox = document.querySelector('.questionText');
+
+observerA.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    observerB.checked = false;
+    ABase = [A.x, A.y];
+    B = B.mult(-1);
+  } else {
+    ABase = [];
+  }
+});
+observerB.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    observerA.checked = false;
+    BBase = [B.x, B.y];
+    A = A.mult(-1);
+  } else {
+    BBase = [];
+  }
+});
+previousBtn.addEventListener('mouseenter', function () {
+  previousBtn.style.opacity = '0.67';
+});
+
+// Restore full opacity when mouse moves away
+previousBtn.addEventListener('mouseleave', function () {
+  previousBtn.style.opacity = '0.1';
+});
+
+previousBtn.addEventListener('click', function () {
+  console.log(index);
+  if (index > 0) {
+    index--;
+    console.log(saved);
+    console.log(saved[index]);
+    B = angleDrawer(saved[index][0][0], saved[index][0][1]);
+    A = angleDrawer(saved[index][1][0], saved[index][1][1]);
+
+    if (saved[index][2] != null){
+    questionTextbox.innerText = saved[index][2];
+    }else{
+      questionTextbox.innerText = 'No saved entries';
+    }
+  } else {
+    console.log('least entries');
+  }
+})
+
+forwardBtn.addEventListener('mouseenter', function () {
+  forwardBtn.style.opacity = '0.67';
+});
+
+// Restore full opacity when mouse moves away
+forwardBtn.addEventListener('mouseleave', function () {
+  forwardBtn.style.opacity = '0.1';
+});
+
+forwardBtn.addEventListener('click', function () {
+  console.log(index);
+  if (index < saved.length - 1) {
+    index++;
+    B = angleDrawer(saved[index][0][0], saved[index][0][1]);
+    A = angleDrawer(saved[index][1][0], saved[index][1][1]);
+
+    if (saved[index][2] != null){
+    questionTextbox.innerText = saved[index][2];
+    }else{
+      questionTextbox.innerText = 'No saved entries';
+    }
+
+  } else {
+    console.log('max entries');
+  }
+})
+
+saved = JSON.parse(localStorage.getItem('saved'));
+
+saveBtn.addEventListener('click', function () {
+  index++;
+  saved.push([[vector1Heading.value, vector1Length.value], [vector2Heading.value, vector2Length.value], questionTextbox.innerText]);
+
+  try{
+  localStorage.setItem('saved', JSON.stringify(saved));
+  const savedTools = JSON.parse(localStorage.getItem('saved'));
+console.log(savedTools);
+  }catch(error){
+    console.error(error);
+  }
+  //console.log(saved);
+})
 
 function createStuff(stuff) {
   const createElement = document.createElement('h1');
@@ -43,19 +146,19 @@ keyFrameBtn.addEventListener('click', function () {
 cameraX.addEventListener('input', function () {
   targetCoordinates[0] = Number(cameraX.value);
   animateCameraTo({ x: targetCoordinates[0], y: targetCoordinates[1], z: targetCoordinates[2] }, { x: lastYaw, y: lastPitch, z: lastRoll }, 1);
-  console.log(targetCoordinates);
+ // console.log(targetCoordinates);
 });
 
 cameraY.addEventListener('input', function () {
   targetCoordinates[1] = Number(cameraY.value);
   animateCameraTo({ x: targetCoordinates[0], y: targetCoordinates[1], z: targetCoordinates[2] }, { x: lastYaw, y: lastPitch, z: lastRoll }, 1);
-  console.log(targetCoordinates);
+ // console.log(targetCoordinates);
 });
 
 cameraZ.addEventListener('input', function () {
   targetCoordinates[2] = Number(cameraZ.value);
   animateCameraTo({ x: targetCoordinates[0], y: targetCoordinates[1], z: targetCoordinates[2] }, { x: lastYaw, y: lastPitch, z: lastRoll }, 1);
-  console.log(targetCoordinates);
+  //console.log(targetCoordinates);
 });
 
 function angleDrawer(angleDeg, length = 200) {
@@ -81,19 +184,19 @@ function drawVector(v, colour, offsetX = 0, offsetY = 0, textt = '', angle = 0, 
   noStroke();
   fill('#FFFF00'); // Manim yellow reads better than deeppink on dark bg
   textSize(16);
-  textFont('Georgia'); // serif reads closer to LaTeX than default sans
+  textFont(font); // serif reads closer to LaTeX than default sans
   text(textt, baseX + v.x + offsetX, baseY + v.y + offsetY);
   pop();
 
   // --- Glow pass behind the main line (Manim's soft-highlight look) ---
-  push();
-  strokeCap(ROUND);
-  for (let i = 3; i > 0; i--) {
-    stroke(red(color(colour)), green(color(colour)), blue(color(colour)), 25);
-    strokeWeight(i * 5);
-    line(baseX, baseY, baseX + v.x, baseY + v.y);
-  }
-  pop();
+  //  push();
+  //  strokeCap(ROUND);
+  //  for (let i = 3; i > 0; i--) {
+  //    stroke(red(color(colour)), green(color(colour)), blue(color(colour)), 25);
+  //   strokeWeight(i * 5);
+  //   line(baseX, baseY, baseX + v.x, baseY + v.y);
+  // }
+  // pop();
 
   // --- Crisp main line ---
   push();
@@ -105,13 +208,13 @@ function drawVector(v, colour, offsetX = 0, offsetY = 0, textt = '', angle = 0, 
   pop();
 
   // --- Filled triangular arrowhead (not open lines) ---
-  push();
-  translate(baseX + v.x, baseY + v.y);
-  rotate(v.heading());
-  noStroke();
-  fill(colour);
-  triangle(0, 0, -12, -5, -12, 5);
-  pop();
+  // push();
+  // translate(baseX + v.x, baseY + v.y);
+  // rotate(v.heading());
+  // noStroke();
+  // fill(colour);
+  // triangle(0, 0, -12, -5, -12, 5);
+  // pop();
 
   // --- Angle arc (thin, pastel, slightly glowing) ---
   push();
@@ -135,37 +238,37 @@ function drawVector(v, colour, offsetX = 0, offsetY = 0, textt = '', angle = 0, 
   pop();
 
   // --- Animated dot moving along the vector's heading ---
-  if (!vectorDots[dotId]) {
-    vectorDots[dotId] = 0;
-  }
+  // if (!vectorDots[dotId]) {
+  //   vectorDots[dotId] = 0;
+  // }
 
-  let heading = v.heading();
-  let length = v.mag();
+  // let heading = v.heading();
+  // let length = v.mag();
 
-  vectorDots[dotId] += velocity;
-  if (vectorDots[dotId] > length) {
-    vectorDots[dotId] = 0;
-  }
-
-  let dist = vectorDots[dotId];
-  let dotX = baseX + cos(heading) * dist;
-  let dotY = baseY + sin(heading) * dist;
+  // vectorDots[dotId] += velocity;
+  // if (vectorDots[dotId] > length) {
+  //   vectorDots[dotId] = 0;
+  // }
+  //
+  // let dist = vectorDots[dotId];
+  // let dotX = baseX + cos(heading) * dist;
+  // let dotY = baseY + sin(heading) * dist;
 
   // glowing traveling dot (Manim loves this pulsing-particle effect)
-  push();
-  noStroke();
-  fill(red(color(colour)), green(color(colour)), blue(color(colour)), 60);
-  circle(dotX, dotY, 16);
-  fill(colour);
-  circle(dotX, dotY, 7);
-  pop();
+  //  push();
+  // noStroke();
+  //  fill(red(color(colour)), green(color(colour)), blue(color(colour)), 60);
+  // circle(dotX, dotY, 16);
+  // fill(colour);
+  // circle(dotX, dotY, 7);
+  //  pop();
 }
 
 function preload() {
   font = loadFont('assets/Typographica-Blp5.ttf');
 }
 function setup() {
-  createCanvas(windowWidth - 50, windowHeight, WEBGL);
+  createCanvas(windowWidth - 145, windowHeight, WEBGL);
   //canvas width 1536, 775
   A = angleDrawer(0, 200);
   B = angleDrawer(90, 200);
@@ -176,8 +279,23 @@ function setup() {
   setCamera(mainCam);
 }
 
+
+let R;
+let RHeading;
+addBtn.addEventListener('click', function () {
+  R = p5.Vector.add(A, B);
+  const radians = Math.atan2(R.y, R.x);
+
+  // 2. Convert radians to degrees
+  const degrees = radians * (180 / Math.PI);
+
+  RHeading = degrees * -1;
+  console.log(degrees);
+})
+
 vector1Heading.addEventListener('change', function () {
   A = angleDrawer(vector1Heading.value, vector1Length.value);
+  console.log(A);
 })
 
 vector1Length.addEventListener('change', function () {
@@ -191,11 +309,6 @@ vector2Heading.addEventListener('change', function () {
 vector2Length.addEventListener('change', function () {
   B = angleDrawer(vector2Heading.value, vector2Length.value);
 })
-let R;
-
-addBtn.addEventListener('click', function () {
-  R = p5.Vector.add(A, B);
-})
 
 let projVector;
 
@@ -204,8 +317,13 @@ dotproductBtn.addEventListener('click', function () {
   projVector = B.copy().mult(dot / B.magSq());
 })
 
+function connectVector() {
+
+}
+
+
 function draw() {
-  background(30);
+  background(0);
 
   // Only let the user orbit when no scripted animation is running,
   // otherwise orbitControl() will overwrite the camera we're animating.
@@ -231,15 +349,15 @@ function draw() {
   //  drawVector(R, color(0, 0, 255));
 
   if (A != null) {
-    drawVector(A, color(255, 0, 0), 50, 50, '20deg', 20);
+    drawVector(A, color(255, 0, 0), 10, 0, '120kmh', vector1Heading.value, 1, 'A-connected', BBase[0], BBase[1]);
   }
   if (B != null) {
-    drawVector(B, color(255, 0, 0), 50, 50, '20deg', 20);
+    drawVector(B, color(0, 0, 255), -20, 25, 'neg 50kmh', vector2Heading.value, 1, 'B-connected', ABase[0], ABase[1]);
   }
   if (R != null) {
-    drawVector(R, color(255, 0, 0), 50, 50, '20deg', 20);
+    drawVector(R, color(0, 255, 0), -30, -60, 'Resultant', RHeading);
   }
-    if (projVector != null) {
+  if (projVector != null) {
     drawVector(projVector, color(0, 255, 0), 50, 50, '20deg', 20);
   }
   getCameraOrientation();
@@ -387,7 +505,7 @@ function getCameraOrientation() {
     targetCoordinates[3] = yaw;
     targetCoordinates[4] = pitch;
     targetCoordinates[5] = roll;
-    console.log(yaw, pitch, roll);
+   // console.log(yaw, pitch, roll);
     lastYaw = yaw;
     lastPitch = pitch;
     lastRoll = roll;
